e9e758ccabddf27a5824d348fabbb317af2e9dda
Ian Christie <iankchristie@gmail.com>
2025-04-18 17:34:56 -0600

    Make baseline against training set.

--- baseline.py
+++ baseline.py
@@ -2,12 +2,13 @@ from test_train_sets import *
 import numpy as np
 from evaluate import evaluate
 
+train_set = get_train_set(0) + get_train_set(1) + get_train_set(2)
 test_set = get_test_set()
 
 overall_scores = []
 excitment_scores = []
 
-for i in test_set:
+for i in train_set:
     overall_scores.append(get_score(i, "Overall"))
     excitment_scores.append(get_score(i, "Excited"))
 
@@ -24,9 +25,9 @@ print(mean_overall_guesses)
 print(mean_excited_guesses)
 
 evaluate(overall_numpy, mean_overall_guesses)
-# Mean Absolute Relative Error: 0.0775
+# Mean Absolute Relative Error: 0.0796
 # Pearson: nan (p-value: nan)
 
 evaluate(excited_numpy, mean_excited_guesses)
-# Mean Absolute Relative Error: 0.1064
+# Mean Absolute Relative Error: 0.0978
 # Pearson: nan (p-value: nan)
